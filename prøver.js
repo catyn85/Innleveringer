0d759788b453798492f8a363bb5ce130cbe72a87
catyn85 <catyn85@hotmail.com>
2023-01-30 17:46:13 +0100

    week4

---
+++ prøver.js
@@ -0,0 +1,54 @@
+// Task: Example (demonstrating how to complete a task)
+// Create a function that returns the circumference of a circle given the radius
+function calculateCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+// Task 1
+// Create a list (array) "people" and populate it with the names from https://github.com/MM-203/misc/blob/main/text/names.txt
+const people = [Benedict Thompson, Lennon Kline, Trinity Cox, Angela Willis, Oliwia Walter, Halle Orozco, Gregory Carrillo, Leyla Dalton, Roosevelt Harrington, Antonia Aguilar];
+
+// Task 2
+// Output all the names in the list people
+console.log (people);
+
+// Task 3
+// Output only the longest name in the list people
+function pickLongestName
+const longest = pickLongestName(people);
+  return longest 
+
+// Task 4
+// Output all the names in alphabetical order.
+
+function pickAlphabeticalName (people)
+  pickAlpabeticalName.sort (); 
+  //let alphabetical = pickAlphabeticalName(people);
+    return alphabetical (people)
+
+// Task 5
+// Output all the names in order from longest to shortest last name.
+// NB! last name (not full name)
+function sortByLastName (a ,b) {   //riktig
+  return a.lastName,length - b.lastName;
+
+}
+  push ( {})
+
+// Task 6
+// Fix the following code
+
+people = f(people);
+console.log(people);
+
+function f(ps) {
+  const o = [];
+  for (let i = 0; i < 10; i++) 
+    o.push({ firstName: ps[i].split(" ")[0], lastName: ps[i].split(" ")[1] });
+  return o;
+}
+
+const sortedPeople = f(peopleList);
+
+
+console.log( sortedPeople );
